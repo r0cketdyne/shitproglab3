78a711b3598e135bcb5005cb36d1f192df066af6
Matthew James Stephenson <stephenson@Matthews-MacBook-Pro.local>
2024-01-20 20:53:31 -0500

    I messed up literally by one digit. else I coded the solution perfectly myself; althrough different than Prof Beth

--- shitprog1.py
+++ shitprog1.py
@@ -10,7 +10,7 @@ sal = 50000
 
 sales = (int(input("how much did you make at actual sales?" )))
 if sales >= 100000:
-    bonsales = sales * 0.125
+    bonsales = sales * 0.0125
     print(bonsales +sal)
 else:
    print("your're taking home", sal , "at Tc")
