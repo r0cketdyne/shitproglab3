74e3a5df4930e6c456acb1dae2b9b0817e58331e
Matthew James Stephenson <stephenson@Matthews-MacBook-Pro.local>
2024-01-20 20:45:33 -0500

    shitprog

---
+++ shitprog1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 20 20:37:40 2024
+
+@author: stephenson
+"""
+
+sal = 50000
+
+sales = (int(input("how much did you make at actual sales?" )))
+if sales >= 100000:
+    bonsales = sales * 0.125
+    print(bonsales +sal)
+else:
+   print("your're taking home", sal , "at Tc")
