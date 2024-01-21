1abfc788efb3de5418c326fc464a140dd42f6113
Matthew James Stephenson <stephenson@Matthews-MacBook-Pro.local>
2024-01-20 21:33:19 -0500

    added comments for Prof Beth's code under mine coded from scratch

--- shitprognextq.py
+++ shitprognextq.py
@@ -19,3 +19,16 @@ if avscore >= 70.0:
 else:
         print("Noooo...you failed!")
         
+#Prof Beth's version
+"""
+test1 = int(input("Enter the 1st test grade: "))
+test2 = int(input("Enter the 2nd test grade: "))
+test3 = int(input("Enter the 3rd test grade: "))
+
+average = (test1 + test2 = test3) / 3
+print("your average it", average)
+if average >= 70.0:
+    print("you passed")
+else:
+    print("you failed")
+"""
